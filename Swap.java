df15826b0ee98ec70bd3d32e3946213fc37cb3a1
V3r4-4 <cryptvr939@gmail.com>
2026-09-23 06:59:30 +0300

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -1,1 +1,14 @@
+public  class Swap {
+    public static void main(String[] args) {
+        //Recommended to use a temporary variable
+        int a=10;
+        int b=20;
+        int tem = a;
+        a=b;
+        b=tem;
+        System.out.println("Values of a and b before swapping are a=10 and b=20 ");
+        System.out.println("Value a after swapping is " + a);
+        System.out.println("Value b after swapping is " + b);
+    }
+}
 
